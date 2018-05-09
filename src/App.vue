804ed42e1776913108fc4998f0f34e2c082a0146
nutdanai <nutdanai@github.com>
2018-05-09 20:14:59 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app" class="img-resize">
+    <img src="./assets/product.jpg">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+div.img-resize img {
+  width: 350px;
+  height: 200px;
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <br>
+    <div>
+  <div class="columns">
+  <div class="column"><a>Name Product : <input class="input is-small" type="text" placeholder="ชื่อสินค้า" v-model="nameproduct"></a></div>
+  </div>
+
+  <div class="columns">
+    <div class="column"><a>Price Product : <input class="input is-small" type="text" placeholder="ราคาสินค้า" v-model="priceproduct"></a></div>
+    </div>
+
+  <div class="columns">
+    <div class="column"><a>Number Product : <input class="input is-small" type="text" placeholder="จำนวนสินค้า" v-model="numberproduct"></a></div>
+      </div>
+
+      <div class="columns">
+        <div class="column"><a>picture Product : <input class="input is-small" type="text" placeholder="รูปสินค้า" v-model="pictureproduct"></a></div>
+          </div>
+
+  <a class="button is-danger is-outlined" @click="addproduct()">ตกลง</a>
+  <a class="button is-success is-outlined">หน้าสินค้า</a>
+</div>
+
+</div>
+</template>
+<script>
+import firebase from 'firebase'
+const config = {
+  databaseURL: 'https://product-9323d.firebaseio.com'
+}
+let app = firebase.initializeApp(config)
+let db = app.database()
+let productRef = db.ref('product')
+export default {
+  name: 'HelloWorld',
+  firebase: {
+    product: productRef
+  },
+  data () {
+    return {
+      msg: 'Product Store',
+      nameproduct: '',
+      priceproduct: '',
+      numberproduct: '',
+      pictureproduct: ''
+    }
+  },
+  methods: {
+    addproduct () {
+      let tmp = {
+        nameproduct: this.nameproduct,
+        priceproduct: this.priceproduct,
+        numberproduct: this.numberproduct,
+        pictureproduct: this.pictureproduct
+      }
+      productRef.push(tmp)
+      this.nameproduct = ''
+      this.priceproduct = ''
+      this.numberproduct = ''
+      this.pictureproduct = ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+input {
+    width: 30%;
+}
+</style>
